e666f837f722cbd71d9762060bdda63f8dbee01b
Hemanth Haridas <hhemanth@gmail.com>
2018-11-29 13:08:52 +1100

    handlin extdname in creditline accounts

--- lib/ofx/parser/ofx102.rb
+++ lib/ofx/parser/ofx102.rb
@@ -143,7 +143,8 @@ module OFX
           :ref_number        => element.search("refnum").inner_text,
           :posted_at         => build_date(element.search("dtposted").inner_text),
           :type              => build_type(element),
-          :sic               => element.search("sic").inner_text
+          :sic               => element.search("sic").inner_text,
+          :extdname          => element.search("extdname").inner_text
         })
       end
 
